06e39f74237943a231621d24c32b90585aae4d67
Shauna Smith <smithshauna2014@gmail.com>
2025-05-09 14:30:46 -0500

    Updated to most resent verison

---
+++ app/static/src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- This is where you can list your products -->
+    <p>No products available yet!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  // You can add some basic data or methods if needed
+  data() {
+    return {
+      products: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Add some styling if necessary */
+</style>
