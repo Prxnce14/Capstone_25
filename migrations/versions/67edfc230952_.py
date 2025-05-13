aeee16f514afda2941d4a2cf3d1894344fd25286
Shauna Smith <smithshauna2014@gmail.com>
2025-05-13 12:27:16 -0500

    added migration file

---
+++ migrations/versions/67edfc230952_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 67edfc230952
+Revises: 9a9278e7d944, e875db19f361
+Create Date: 2025-05-13 12:21:03.250974
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67edfc230952'
+down_revision = ('9a9278e7d944', 'e875db19f361')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
